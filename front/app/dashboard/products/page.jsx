84599f74f0d2b4be6d7f6acf6630bb0250a8b6c0
Johansen <Johamaster@gmail.com>
2024-07-15 22:29:01 -0400

    feat: Agregando modulos users y product

--- front/app/dashboard/products/page.jsx
+++ front/app/dashboard/products/page.jsx
@@ -1,5 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/app/components/dashboard/products/products.module.css"
+import Search from "@/app/components/dashboard/search/page";
+import Pagination from "@/app/components/dashboard/pagination/page";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return <div className={styles.container}>
+  <div className={styles.top}>
+    <Search placeholder="Buscar un Producto..." />
+    <Link href="/dashboard/products/add">
+      <button className={styles.addButton}>Agregar Producto</button>
+    </Link>
+  </div>
+  <table className={styles.table}>
+    <thead>
+      <tr>
+        <td>Titulo</td>
+        <td>Descripcion</td>
+        <td>Precio</td>
+        <td>Creado Por</td>
+        <td>Stock</td>
+        <td>Accion</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <div className={styles.product}>
+            <Image
+              src="/noproduct.jpg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.productImage}
+            />
+            Iphone
+          </div>
+        </td>
+        <td>Desc</td>
+        <td>$ 999</td>
+        <td>14.07.24</td>
+        <td>72</td>
+        <td>
+          <div className={styles.buttons}>
+            <Link href="/">
+              <button className={`${styles.button} ${styles.view}`}>
+                Info
+              </button>
+            </Link>
+            <button className={`${styles.button} ${styles.delete}`}>
+              Borrar
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <Pagination/>
+</div>;
 };
 
 export default ProductsPage;
--- front/app/dashboard/users/page.jsx
+++ front/app/dashboard/users/page.jsx
@@ -1,5 +1,65 @@
+import styles from "@/app/components/dashboard/users/users.module.css";
+import Search from "@/app/components/dashboard/search/page";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "@/app/components/dashboard/pagination/page";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Buscar un Usuario..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Agregar Usuario</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Nombre</td>
+            <td>Email</td>
+            <td>Creado</td>
+            <td>Roll</td>
+            <td>Estado</td>
+            <td>Accion</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.users}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Johansen Picado
+              </div>
+            </td>
+            <td>Johamaster@gmail.com</td>
+            <td>14.07.24</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    Info
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Borrar
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination/>
+    </div>
+  );
 };
 
 export default UsersPage;
