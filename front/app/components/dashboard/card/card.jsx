bf3587fbdfc4f770f30adaa5cd39b61520d4a4c6
Johansen <Johamaster@gmail.com>
2024-07-19 22:56:47 -0400

    feat: Modification Rendecards

--- front/app/components/dashboard/card/card.jsx
+++ front/app/components/dashboard/card/card.jsx
@@ -3,7 +3,7 @@ import styles from "./card.module.css";
 const Card = ({ icon: Icon, title, number, detail, change }) => {
   return (
     <div className={styles.container}>
-      <MdSupervisedUserCircle size={24} />
+      <Icon size={24} />
       <div className={styles.texts}>
         <span className={styles.title}> Total Users </span>
         <span className={styles.number}>10.275</span>
--- front/app/dashboard/page.jsx
+++ front/app/dashboard/page.jsx
@@ -2,7 +2,7 @@ import Card from "../components/dashboard/card/card";
 import Chart from "../components/dashboard/chart/chart";
 import styles from "../components/dashboard/dashboard.module.css";
 import Transactions from "../components/dashboard/transactions/transactions";
-import RendeCards from "../components/dashboard/card/RendeCards"
+
 
 
 const Dashboard = () => {
