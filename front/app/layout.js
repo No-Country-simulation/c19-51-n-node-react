bda4191cce3b18e1531b96e3f715bf5f008f6858
Vitale Tobias <vitaletobias1@gmail.com>
2024-07-27 12:25:11 -0300

    Merge branch 'main' of https://github.com/No-Country-simulation/c19-51-n-node-react

--- front/app/layout.js
+++ front/app/layout.js
@@ -4,13 +4,14 @@ import "./components/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NC PROYECT",
+  title: "InvenTrack",
   description: "Dashboard NC",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/image.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
