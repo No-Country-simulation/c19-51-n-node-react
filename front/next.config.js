59c82f9d4103a925f43c1ed7a310bb754f723a25
Johansen <Johamaster@gmail.com>
2024-07-31 09:12:16 -0400

    aceptando cambios

--- front/next.config.js
+++ front/next.config.js
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    images: {
-      domains: ['example.com', 'images.pexels.com', 'i.postimg.cc'],
-    },
-  }
-  
-  module.exports = nextConfig
-
-  
-=======
   images: {
     domains: ['example.com', 'images.pexels.com', 'i.postimg.cc'],
   },
 }
 
 module.exports = nextConfig
->>>>>>> dashboard
